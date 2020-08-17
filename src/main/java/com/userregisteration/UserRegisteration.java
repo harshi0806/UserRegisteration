0ebe68780de5338af60e70506b7e9c1e6456ff08
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-17 23:12:32 +0530

    Refactored:Validate Email format

---
+++ src/main/java/com/userregisteration/UserRegisteration.java
@@ -0,0 +1,17 @@
+package com.userregisteration;
+import java.util.regex.Pattern;
+
+public class UserRegisteration {
+    //Pattern used for both first name and last name
+    private static final String NAME_PATTERN = "^([A-Z][a-zA-Z]{2,})$";
+    private static final String EMAIL_PATTERN = "^[0-9A-Za-z]+([._+-][0-9a-zA-Z]+)*@[0-9a-zA-Z]+.[0-9a-zA-Z]{2,4}([.][a-zA-Z]{2})*$";
+
+    public boolean validateName(String Name) {
+        Pattern pattern =Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(Name).matches();
+    }
+    public boolean validateEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        return pattern.matcher(email).matches();
+    }
+}
