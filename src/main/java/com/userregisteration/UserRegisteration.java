af394a11a2b7db8e016eb1e7903a5b107e55065e
harshi0806 <harshitashrivatav121@gmail.com>
2020-08-17 22:21:29 +0530

    Added:Validate last name

---
+++ src/main/java/com/userregisteration/UserRegisteration.java
@@ -0,0 +1,12 @@
+package com.userregisteration;
+import java.util.regex.Pattern;
+
+public class UserRegisteration {
+    //Pattern used for both first name and last name
+    private static final String NAME_PATTERN = "^([A-Z][a-zA-Z]{2,})$";
+
+    public boolean validateName(String Name) {
+        Pattern pattern =Pattern.compile(NAME_PATTERN);
+        return pattern.matcher(Name).matches();
+    }
+}
